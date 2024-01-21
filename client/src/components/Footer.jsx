3bfa6c12ed0a13beaa01f95a6b4f7287cba8d683
Marc Granier <mgranier@comcast.net>
2024-01-21 18:32:20 -0500

    Begin Footer Component

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,7 +1,31 @@
-import React from 'react';
+import {
+	Button,
+	ButtonGroup,
+	Container,
+	Divider,
+	IconButton,
+	Input,
+	Stack,
+	Text,
+	useColorMode as mode,
+	Box,
+	Flex,
+	Icon,
+} from '@chakra-ui/react';
+import { FaGithub, FaLinkedin, FaFacebook } from 'react-icons/fa';
+import { BsPhoneFlip } from 'react-icons/bs';
 
-const Footer = () => {
-	return <div>Footer</div>;
-};
+const Footer = () => (
+	<Box width='100%' bg={mode('cyan.300', 'gray.300')}>
+		<Container as='footer' maxW='7xl'>
+			<Stack
+				spacing='8'
+				direction={{ base: 'column', md: 'row' }}
+				justify='space-between'
+				py={{ base: '12', md: '16' }}
+			></Stack>
+		</Container>
+	</Box>
+);
 
 export default Footer;
