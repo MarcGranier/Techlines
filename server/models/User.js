2cc00e7a914d22696ec6f3526cdde4c3b486c777
Marc Granier <mgranier@comcast.net>
2024-01-22 14:30:04 -0500

    Complete Login

--- server/models/User.js
+++ server/models/User.js
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose';
-import bcrypt from 'bgryptjs';
+import bcrypt from 'bcryptjs';
 
 const userSchema = new mongoose.Schema(
 	{
@@ -28,6 +28,5 @@ userSchema.pre('save', async function (next) {
 	this.password = await bcrypt.hash(this.password, salt);
 });
 
-const User = mongoose.model('User, userSchema');
-
+const User = mongoose.model('User', userSchema);
 export default User;
