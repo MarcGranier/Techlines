e55f1fb7ecad77d118e934f2f37570f936dc62aa
Marc Granier <mgranier@comcast.net>
2024-01-23 13:48:28 -0500

    Test Verification email sent

--- server/middleware/sendVerificationEmail.js
+++ server/middleware/sendVerificationEmail.js
@@ -1,14 +1,38 @@
 import nodemailer from 'nodemailer';
 
-export const sendVerificationEmail = (TokenExpiredError, email, id) => {
+export const sendVerificationEmail = (token, email, name, id) => {
 	const html = `
   <html>
     <body>
       <h3>Dear ${name}</h3>
-      <p>Thanks for signing up at Tech Lines</p>
+      <p>Thanks for signing up at Tech Lines!</p>
       <p>Use the link below to verify your email</p>
       <a href='http://localhost:3000/email-verify/${token}'>Click here!</a>
     </body>  
   </html>
   `;
+
+	const transporter = nodemailer.createTransport({
+		service: 'gmail',
+		auth: {
+			user: 'marcgranier969@gmail.com',
+			pass: process.env.APP_PASSWORD,
+		},
+	});
+
+	const mailOptions = {
+		from: 'marcgranier969@gmail.com',
+		to: email,
+		subject: 'Verify your email address',
+		html: html,
+	};
+
+	transporter.sendMail(mailOptions, function (error, info) {
+		if (error) {
+			console.log(error);
+		} else {
+			console.log(`Email sent to ${email}`);
+			console.log(info.response);
+		}
+	});
 };
