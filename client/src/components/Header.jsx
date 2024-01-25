db59b9d99a4b48faf10a45a9f79ec1e1601c0623
Marc Granier <mgranier@comcast.net>
2024-01-25 14:49:13 -0500

    Complete Header Functionality & Test Successfully

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -32,7 +32,7 @@ import { toggleFavorites } from '../redux/actions/productActions';
 import NavLink from './NavLink';
 import ColorModeToggle from './ColorModeToggle';
 import { BiUserCheck, BiLogInCircle } from 'react-icons/bi';
-import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons';
+import { HamburgerIcon, CloseIcon, ChevronDownIcon } from '@chakra-ui/icons';
 import { TbShoppingCart } from 'react-icons/tb';
 import { logout } from '../redux/actions/userActions';
 import { MdOutlineAdminPanelSettings } from 'react-icons/md';
@@ -47,11 +47,21 @@ const Links = [
 const Header = () => {
 	const { isOpen, onOpen, onClose } = useDisclosure();
 	const dispatch = useDispatch();
+	const toast = useToast();
 	const { favoritesToggled } = useSelector((state) => state.product);
 	const { cartItems } = useSelector((state) => state.cart);
 	const { userInfo } = useSelector((state) => state.user);
 
 	useEffect(() => {}, [favoritesToggled, dispatch]);
+
+	const logoutHandler = () => {
+		dispatch(logout());
+		toast({
+			description: 'You have been logged out.',
+			status: 'success',
+			isClosable: 'true',
+		});
+	};
 
 	return (
 		<Box bg={mode(`cyan.300`, `gray.900`)} px='4'>
@@ -138,7 +148,75 @@ const Header = () => {
 					</HStack>
 				</HStack>
 				<Flex alignItems='center'>
-					<BiUserCheck />
+					{userInfo ? (
+						<Menu>
+							<MenuButton
+								rounded='full'
+								variant='link'
+								cursor='pointer'
+								minW='0'
+							>
+								<HStack>
+									<BiUserCheck size='30px' />
+									<ChevronDownIcon />
+								</HStack>
+							</MenuButton>
+							<MenuList>
+								<HStack>
+									<Text pl='3' as='i'>
+										{userInfo.email}
+									</Text>
+								</HStack>
+								<Divider py='1' />
+								<MenuItem as={ReactLink} to='/order-history'>
+									Order History
+								</MenuItem>
+								<MenuItem as={ReactLink} to='/profile'>
+									Profile
+								</MenuItem>
+								{userInfo.isAdmin && (
+									<>
+										<MenuDivider />
+										<MenuItem as={ReactLink} to='/admin-console'>
+											Admin Console
+										</MenuItem>
+									</>
+								)}
+								<MenuDivider />
+								<MenuItem onClick={logoutHandler}>Logout</MenuItem>
+							</MenuList>
+						</Menu>
+					) : (
+						<Menu>
+							<MenuButton
+								as={IconButton}
+								variant='ghost'
+								cursor='pointer'
+								icon={<BiLogInCircle size='25px' />}
+							/>
+							<MenuList>
+								<MenuItem
+									as={ReactLink}
+									to='/login'
+									p='2'
+									fontWeight='400'
+									variant='link'
+								>
+									Sign in
+								</MenuItem>
+								<MenuDivider />
+								<MenuItem
+									as={ReactLink}
+									to='/registration'
+									p='2'
+									fontWeight='400'
+									variant='link'
+								>
+									Sign up
+								</MenuItem>
+							</MenuList>
+						</Menu>
+					)}
 				</Flex>
 			</Flex>
 			<Box display='flex'>
