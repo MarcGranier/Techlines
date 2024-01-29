354352bf6feb6d10bdbe858b4445be0e1dc7ec01
Marc Granier <mgranier@comcast.net>
2024-01-29 14:40:24 -0500

    Complete Whole Registration and Sign In Functionality

--- server/middleware/sendVerificationEmail.js
+++ server/middleware/sendVerificationEmail.js
@@ -1,29 +1,28 @@
 import nodemailer from 'nodemailer';
-
+// vnch hliv npcv divl
+// benstechlines1@gmail.com
 export const sendVerificationEmail = (token, email, name) => {
 	const html = `
-  <html>
-    <body>
-      <h3>Dear ${name}</h3>
-      <p>Thanks for signing up at Tech Lines!</p>
-      <p>Use the link below to verify your email</p>
-      <a href="http://localhost:3000/email-verify/${token}">Click here!</a>
-    </body>  
-  </html>
-  `;
-
-	const appPass = process.env.APP_PASSWORD;
+    <html>
+        <body>
+            <h3>Dear ${name}</h3>
+            <p>Thanks for signing up at Tech Lines!</p>
+            <p>Use the link below to verify your email</p>
+            <a href="http://localhost:3000/email-verify/${token}">Click here!</a>
+        </body>
+    </html>
+    `;
 
 	const transporter = nodemailer.createTransport({
 		service: 'gmail',
 		auth: {
-			user: 'techlines03@gmail.com',
-			pass: appPass,
+			user: 'benstechlines1@gmail.com',
+			pass: 'vnch hliv npcv divl',
 		},
 	});
 
 	const mailOptions = {
-		from: 'techlines03@gmail.com',
+		from: 'benstechlines1@gmail.com',
 		to: email,
 		subject: 'Verify your email address',
 		html: html,
@@ -33,7 +32,7 @@ export const sendVerificationEmail = (token, email, name) => {
 		if (error) {
 			console.log(error);
 		} else {
-			console.log(`Email sent to ${email}`);
+			console.log(`Email send to ${email}`);
 			console.log(info.response);
 		}
 	});
